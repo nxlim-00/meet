80eee54324e46e0cd4f225d70a2735c5d660f067
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-05 12:23:28 +0200

    Updated file
    Added array to allLocations
    Added handleItemClicked function

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const CitySearch = (allLocations) => {
+const CitySearch = ({ allLocations = [] }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
@@ -15,6 +15,11 @@ const CitySearch = (allLocations) => {
     setQuery(value);
     setSuggestions(filteredLocations);
   };
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+  };
   return (
     <div id="city-search">
       <input
@@ -28,9 +33,13 @@ const CitySearch = (allLocations) => {
       {showSuggestions ? (
         <ul className="suggestions">
           {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>;
+            return (
+              <li onClick={handleItemClicked} key={suggestion}>
+                {suggestion}
+              </li>
+            );
           })}
-          <li key="See all cities">
+          <li key="See all cities" onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
