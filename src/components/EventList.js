ddcb8a70ece5549d17cac76a2b89419257f38d55
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-06 12:56:42 +0200

    Removed lines

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,19 +1,5 @@
 import Event from './Event';
 
-// OPTION 1: add empty array when prop is extracted
-/* const EventList = ({ events = [] }) => {
-  return (
-    <ul id="event-list">
-      {events.map((event) => (
-        <Event event={event} />
-      ))}
-    </ul>
-  );
-};
-
-export default EventList; */
-
-// OPTION 2: only call map function when "events" is declared
 const EventList = ({ events }) => {
   return (
     <ul id="event-list">
