157399c64c03b84cfce84e61adcabce3d477e266
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-06 12:56:27 +0200

    Updates file

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -2,27 +2,20 @@ import { useState } from 'react';
 
 const NumberOfEvents = () => {
   const [number, setNumber] = useState(32);
-  const [newValue, setNewValue] = useState('');
-  const [isChanged, setIsChanged] = useState(false);
 
-  const handleInputChanged = (e) => {
-    setNewValue(e.target.value);
-    setIsChanged(true);
-
-    if (e.target.value === '') {
-      setNumber(0);
-    } else {
-      setNumber(e.target.value);
-    }
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setNumber(value);
   };
 
   return (
     <div id="numberOfevents">
-      <label>Number of Events:</label>
+      <label htmlFor="numberOfeventsInput">Number of Events: </label>
       <input
         type="text"
-        className="number"
-        value={isChanged ? newValue : number}
+        id="numberOfeventsInput"
+        className="numberOfeventsInput"
+        value={number}
         onChange={handleInputChanged}
       />
     </div>
