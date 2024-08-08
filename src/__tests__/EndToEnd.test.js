ec01782274a3f456f25f876cef37968af86fc1c8
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-08 15:01:42 +0200

    Added tests for feature filter events by city

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -38,3 +38,48 @@ describe('show/hide event details', () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+describe('Filter events by city.', () => {
+  let browser;
+  let page;
+  let eventListItems;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 50, // slow down by 250ms
+      timeout: 0, // disables any timeout limitations.
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+    eventListItems = await page.$$('.event');
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test("When user hasn't searched for a city, show upcoming events from all cities.", async () => {
+    expect(eventListItems.length).toBe(32);
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+    await page.type('.city', 'Berlin');
+    const suggestionListItems = await page.$$('.suggestions li');
+    expect(suggestionListItems.length).toBe(2);
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    for (let i = 0; i < 6; i++) {
+      await page.keyboard.press('Backspace');
+    }
+    await page.type('.city', 'Berlin');
+    const suggestionText = await page.$eval(
+      '.suggestions li:first-child',
+      (el) => el.textContent
+    );
+    await page.click('.suggestions li:first-child');
+    const citySearchInputValue = await page.$eval('.city', (el) => el.value);
+    expect(citySearchInputValue).toBe(suggestionText);
+  });
+});
