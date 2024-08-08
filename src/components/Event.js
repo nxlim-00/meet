5375b7168467217f63b21e0e703b7ed588ae8338
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-08 18:53:52 +0200

    Added container

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,22 +3,24 @@ import { useState } from 'react';
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li className="event">
-      <h2 className="eventHeader">{event && event.summary}</h2>
-      <p>{event && event.location}</p>
-      <p>{event && new Date(event.created).toUTCString()}</p>
-      {showDetails ? (
-        <p className="details">{event && event.description}</p>
-      ) : null}
-      <button
-        className="details-btn"
-        onClick={() => {
-          showDetails ? setShowDetails(false) : setShowDetails(true);
-        }}
-      >
-        {showDetails ? 'hide details' : 'show details'}
-      </button>
-    </li>
+    <div className="event-container">
+      <li className="event">
+        <h2 className="eventHeader">{event && event.summary}</h2>
+        <p>{event && event.location}</p>
+        <p>{event && new Date(event.created).toUTCString()}</p>
+        {showDetails ? (
+          <p className="details">{event && event.description}</p>
+        ) : null}
+        <button
+          className="details-btn"
+          onClick={() => {
+            showDetails ? setShowDetails(false) : setShowDetails(true);
+          }}
+        >
+          {showDetails ? 'hide details' : 'show details'}
+        </button>
+      </li>
+    </div>
   );
 };
 
