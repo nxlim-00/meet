847f86f73c1c2fb3750d18ea0a21cae52580e07c
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-15 21:15:12 +0200

    Created EventGenreChart

---
+++ src/components/EventGenreChart.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+} from 'recharts';
+
+const EventGenreChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+  const colors = ['#DD0000', '#00DD00', '#0000DD', '#DDDD00', '#DD00DD'];
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      ).length;
+      return { name: genre, value: filteredEvents };
+    });
+    return data;
+  };
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#068488"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#068488"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={150}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenreChart;
